c6b05c6ac17c4540bf80792a69d796f53e4a9d93
oguzk234 <oguzk234@gmail.com>
2023-11-10 16:03:52 +0300

    çarpma animasyonu

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,12 +8,17 @@ public class CameraController : MonoBehaviour
     [SerializeField] float aheadSpeed;
     [SerializeField] float followDamping;
     [SerializeField] float cameraHeight;
+    [SerializeField] Vector3 targetPosition;
     [SerializeField] Vector3 offset;
+
+
+    [SerializeField] Vector3 ShakeAmount;
 
     Rigidbody observeableRigidBody;
     void Start()
     {
         observeableRigidBody=observeable.GetComponent<Rigidbody>();
+        //ShakeAmount = new Vector3(0, 0, 30);
 
     }
 
@@ -22,8 +27,27 @@ public class CameraController : MonoBehaviour
     {
         if (observeable == null) return;
         
-        Vector3 targetPosition = observeable.position + Vector3.up * cameraHeight + observeableRigidBody.velocity * aheadSpeed;
-        transform.position = Vector3.Lerp(transform.position , targetPosition + offset, followDamping * Time.deltaTime);
+        targetPosition = observeable.position + Vector3.up * cameraHeight + observeableRigidBody.velocity * aheadSpeed;
+        transform.position = Vector3.Lerp(transform.position , targetPosition + offset + ShakeAmount, followDamping * Time.deltaTime);
     }
 
+    public IEnumerator Shake(float duration, float magnitude)
+    {
+
+        float elapsed = 0f;
+
+        while(elapsed < duration)
+        {
+            float x = Random.Range(-2f, 1f) * magnitude;
+            float z = Random.Range(-1f, 1f) * magnitude;
+
+            ShakeAmount = new Vector3(x, 0 ,z);
+
+            elapsed += Time.deltaTime;
+
+            yield return null;
+        }
+
+        ShakeAmount = Vector3.zero;
+    }
 }
